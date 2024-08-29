465df132eb5ada57f72119c052e45ffa68edc042
Leandro Tami <leandro.tami@gmail.com>
2024-08-29 16:19:39 -0300

    Initial Commit

---
+++ mediabase/mediabaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  mediabaseApp.swift
+//  mediabase
+//
+//  Created by Leandro on 29/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct mediabaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
